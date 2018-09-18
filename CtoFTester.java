4a4c840ed6a23fc5e60c0e256a64a69147ff7ee9
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-09-18 19:13:36 -0400

    Function #2 Tested. Further Changes to be Made

--- CtoFTester.java
+++ CtoFTester.java
@@ -3,13 +3,15 @@ public static double CelsiustoFahrenheit(double C){
   return C * 1.8 + 32.0;
 }
 
+public static double FahrenheittoCelsius(double F){
+  return (F - 32.0) / 1.8;
+}
+
 public static void main(String[] args) {
  System.out.println(CelsiustoFahrenheit(10.0));
  System.out.println(CelsiustoFahrenheit(9.0));
-}
-
-public static double FahrenheittoCelsius(double F){
-  return (F - 32.0) / 1.8;
+ System.out.println(FahrenheittoCelsius(50.0));
+ System.out.println(FahrenheittoCelsius(48.2));
 }
 
 }
