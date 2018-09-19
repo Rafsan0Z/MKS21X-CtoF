bdab75f157c61d305ac9a7d310157f35a144d54f
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-09-19 15:41:23 -0400

    Change function name

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,17 +1,17 @@
 public class CtoFTester{
-public static double CelsiustoFahrenheit(double C){
+public static double celsiustoFahrenheit(double C){
   return C * 1.8 + 32.0;
 }
 
-public static double FahrenheittoCelsius(double F){
+public static double fahrenheittoCelsius(double F){
   return (F - 32.0) / 1.8;
 }
 
 public static void main(String[] args) {
- System.out.println(CelsiustoFahrenheit(10.0));
- System.out.println(CelsiustoFahrenheit(9.0));
- System.out.println(FahrenheittoCelsius(50.0));
- System.out.println(FahrenheittoCelsius(48.2));
+ System.out.println(celsiustoFahrenheit(10.0));
+ System.out.println(celsiustoFahrenheit(9.0));
+ System.out.println(fahrenheittoCelsius(50.0));
+ System.out.println(fahrenheittoCelsius(48.2));
 }
 
 }
